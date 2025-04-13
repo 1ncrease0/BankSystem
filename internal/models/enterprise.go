81663667f5c223918900614c83e5f41e7f79fcfb
Dmitry Snesar <143742954+1ncrease0@users.noreply.github.com>
2025-04-13 12:43:28 +0300

    Update enterprise.go

--- internal/models/enterprise.go
+++ internal/models/enterprise.go
@@ -1,11 +1,11 @@
 package models
 
 type Enterprise struct {
-	Id           int    `db:"id"`            // Уникальный идентификатор предприятия
-	Type         string `db:"type"`          // Тип предприятия (ИП, ООО, ЗАО и т.д.)
-	LegalName    string `db:"legal_name"`    // Юридическое название предприятия
-	UNP          string `db:"unp"`           // УНП (уникальный номер предприятия)
-	BIC          string `db:"bic"`           // БИК банка предприятия
-	LegalAddress string `db:"legal_address"` // Юридический адрес
-	BankID       int    `db:"bank_id"`       // Идентификатор банка, в котором зарегистрировано предприятие
+	Id           int    `db:"id"`          
+	Type         string `db:"type"`          
+	LegalName    string `db:"legal_name"`   
+	UNP          string `db:"unp"`          
+	BIC          string `db:"bic"`          
+	LegalAddress string `db:"legal_address"`
+	BankID       int    `db:"bank_id"`       
 }
